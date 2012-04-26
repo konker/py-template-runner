a694ad2b543b0da4d29ea288b8e89dfaf02106c1
konker <konker@gmail.com>
2012-04-26 12:41:02 +0100

    Changed so that template filename argument automatically provides a TemplateLookup resolver

--- template-runner.py
+++ template-runner.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
+import os
 import sys
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
 """
-Read stdin as a Mako template and render it to stdout
+Read the file passed as the first argument as a Mako template and render it to stdout
 """
 def main():
-    lu = TemplateLookup(directories=['in'])
-    t = lu.get_template(sys.argv[1])
+    # get the path to the template as a cli argument
+    p = sys.argv[1]
+
+    # split into directory/filename
+    dn = os.path.dirname(p)
+    fn = os.path.basename(p)
+
+    # set up the lookup resolver for templates/includes
+    lu = TemplateLookup(directories=[dn])
+
+    # read the template and render to stdout
+    t = lu.get_template(fn)
     sys.stdout.write(t.render())
+
 
 if __name__ == '__main__':
     main()
